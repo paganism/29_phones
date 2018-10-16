0cfbd2d6530867a04e494fc5d54a16865170b663
YuriyB <paganism@mail.ru>
2018-10-16 23:25:48 +0300

    repaired marks

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql://score:Rysherat2@localhost:5432/shop'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CHECK_NEW_ORDERS_TIMEOUT = 10
+
