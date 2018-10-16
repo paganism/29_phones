ae97d3421b9419716f5a8fded5858c40ca1d2b6e
yuriy_beloff <paganismrus@gmail.com>
2018-10-16 14:28:46 +0300

    added phone normalizing script and requirements

--- phone_processing.py
+++ phone_processing.py
@@ -1,33 +1,17 @@
 import re
+import phonenumbers
 
 
-def normalize_phone(phone_number):
-    if phone_number:
-        norm_mob = re.sub(r'(\s+)?[+]?[-]?', '', phone_number)
-        print(norm_mob)
-        # проверяем строку на наличие в ней только необходимых символов
-        numeric_phone = re.findall(r'[\d]', norm_mob)
-        print(numeric_phone[0])
-        if numeric_phone[0] in ['7', '8']:
-            print('NUMERIC PHONE ', numeric_phone[1:])
-            print(''.join(numeric_phone[1:]))
-        else:
-            print('HVG')    
-        # если количество знаков в двух строках совпадает, значит это номер телефона
-        if (len(numeric_phone) == len(norm_mob)) and (len(norm_mob) >= 10):
-            rev_norm_mob = norm_mob[::-1]
-            print(rev_norm_mob)
-            #norm_mob = rev_norm_mob[0:10]
-            
-            print(type((norm_mob)))
-            # if norm_mob[::-1][0] == '9':
-            #     print(norm_mob[::-1])
-            #     return norm_mob[::-1]
-    else:
-        print(phone_number)
-        return False
+def normalize_phone(raw_number):
+    norm_mob = re.sub(r'(\D+)', '', raw_number)
+    try:
+        parsed_number = phonenumbers.parse(norm_mob, 'RU')
+        if phonenumbers.is_valid_number(parsed_number):
+            return parsed_number.national_number
+    except NumberParseException:
+        return raw_number
 
 
 if __name__ == "__main__":
-    print(normalize_phone('8 929 111-22-33'))
-    print(normalize_phone('+79291112266'))
+    normalize_phone('8 929 111-22-33')
+    normalize_phone('+79291112266')
