5554d8dc460df90b20456318ec2b090b705c8902
YuriyB <paganism@mail.ru>
2018-10-20 13:16:38 +0300

    added SQLALCHEMY_POOL_RECYCLE param

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ import os
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql://score:Rysherat2@localhost:5432/shop'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CHECK_NEW_ORDERS_TIMEOUT = 10
-
+    CHECK_NEW_ORDERS_TIMEOUT = 15
+    SQLALCHEMY_POOL_RECYCLE = 3
+    
